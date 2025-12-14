154121e9ae6b33fd3297b44c7ac80758fcb21486
comptenintendoamericain-rgb <comptenintendoamericain@gmail.com>
2025-12-14 18:49:30 +0100

    changement du mouvement du rat

--- de-rat-pigeon/player.gd
+++ de-rat-pigeon/player.gd
@@ -5,24 +5,31 @@ extends CharacterBody2D
 @export var gravity = 6000
 @export_range(0.0, 1.0) var friction = 0.1
 @export_range(0.0 , 1.0) var acceleration = 0.25
-@onready var camera := $Camera as Camera2D
 
 func _physics_process(delta):
-    velocity.y += gravity * delta
-    var dir = Input.get_axis("walk_left", "walk_right")
-    if dir != 0:
-        velocity.x = lerp(velocity.x, dir * speed, acceleration)
-        $AnimatedSprite2D.set_animation("run")
-    else:
-        velocity.x = lerp(velocity.x, 0.0, friction)
-        #$AnimatedSprite2D.set_animation("default")
+	velocity.y += gravity * delta
+	var dir = Input.get_axis("walk_left", "walk_right")
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
 
-    move_and_slide()
-    if Input.is_action_just_pressed("jump") and is_on_floor():
-        velocity.y = jump_speed
-        $AnimatedSprite2D.set_animation("jump") # ca ne marche pas
-        rotation_degrees = 0.
-    if Input.is_action_just_pressed("jump") and is_on_wall():
-        velocity.y = jump_speed
-        $AnimatedSprite2D.set_animation("run")
-        rotation_degrees = 90.
+	else:
+		velocity.x = lerp(velocity.x, 0.0, friction)
+		#$AnimatedSprite2D.set_animation("default")
+	if absf(velocity.x) > 0.4:
+		$AnimatedSprite2D.play("run")
+	if absf(velocity.x) < 0.4 :
+		$AnimatedSprite2D.play("default")
+	move_and_slide()
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+		$AnimatedSprite2D.set_animation("jump") # ca ne marche pas
+		rotation_degrees = 0.
+
+		
+	if Input.is_action_just_pressed("jump") and is_on_wall():
+		velocity.y = jump_speed
+		if absf(velocity.x) > 0.1:
+			$AnimatedSprite2D.play("run")
+		rotation_degrees = -90.
+	elif is_on_floor():
+		rotation_degrees = 0.
