0196c804c85b3f8d82ff0e922797b021d3725e82
comptenintendoamericain-rgb <comptenintendoamericain@gmail.com>
2025-12-14 16:34:04 +0100

    Suivi de la caméra et wall jump

--- de-rat-pigeon/player.gd
+++ de-rat-pigeon/player.gd
@@ -1,19 +1,22 @@
 extends CharacterBody2D
 
-@export var speed = 10
-@export var jump_speed = -180
-@export var gravity = 400
+@export var speed = 800
+@export var jump_speed = -1800
+@export var gravity = 6000
 @export_range(0.0, 1.0) var friction = 0.1
 @export_range(0.0 , 1.0) var acceleration = 0.25
-
+@onready var camera := $Camera as Camera2D
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	var dir = Input.get_axis("walk_left", "walk_right")
 	if dir != 0:
-		position.x += dir * speed
-
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, friction)
 
 	move_and_slide()
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y += jump_speed
+		velocity.y = jump_speed
+	if Input.is_action_just_pressed("jump") and is_on_wall():
+		velocity.y = jump_speed
