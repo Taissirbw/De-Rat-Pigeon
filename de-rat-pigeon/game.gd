b932b8f5a3d091600c0d4bb8469a948d2d4e4361
Taïssir <taissir.marce@gmail.com>
2025-12-14 15:29:14 +0100

    Resize asset

--- de-rat-pigeon/game.gd
+++ de-rat-pigeon/game.gd
@@ -1,16 +1,8 @@
 class_name Game extends Node
 
 
-@onready var _pause_menu := $InterfaceLayer/pause_menu as PauseMenu
+#@onready var _pause_menu := $InterfaceLayer/pause_menu as PauseMenu
 
 
 func _unhandled_input(event: InputEvent) -> void:
-
-	if event.is_action_pressed(&"toggle_pause"):
-		var tree := get_tree()
-		tree.paused = not tree.paused
-		if tree.paused:
-			_pause_menu.open()
-		else:
-			_pause_menu.close()
-		get_tree().root.set_input_as_handled()
+	pass
