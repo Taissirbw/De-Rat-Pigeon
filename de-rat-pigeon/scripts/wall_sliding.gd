4fb5d20e892dfdb5d04ea130723c8b66e509f2eb
Marce <taissir.marce@u-bordeaux.fr>
2026-03-12 23:15:08 +0100

    wall_slide j'essaie de le faire marcher

--- de-rat-pigeon/scripts/wall_sliding.gd
+++ de-rat-pigeon/scripts/wall_sliding.gd
@@ -55,7 +55,23 @@ func physics_update(delta: float) -> void:
 
 		# Mets à jour le sens des sprites
 		#player.animation_player.flip_h = ! bool(player.look_dir_x)
-		player.rotation_degrees = -90. * player.look_dir_x
+		#player.rotation_degrees = -90. * player.look_dir_x
+		if player.look_dir_x == 1:
+			player.rotation_degrees = -90.
+			if sign(player.velocity.y) == 1:
+				# Le rat est sur un mur de droite, vers le bas
+				player.scale.x = -1.
+			elif sign(player.velocity.y) == -1:
+				# Le rat est sur un mur de droite, vers le haut
+				player.scale.x = 1.
+		elif player.look_dir_x == -1:
+			player.rotation_degrees = 90.
+			if sign(player.velocity.y) == 1:
+				# Le rat est sur un mur de gauche, vers le bas
+				player.scale.x = 1.
+			elif sign(player.velocity.y) == -1:
+				# Le rat est sur un mur de gauche, vers le haut
+				player.scale.x = -1.
 
 
 		# jsp ce que ça fait
