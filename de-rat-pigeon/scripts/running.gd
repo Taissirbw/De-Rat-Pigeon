e2831fba56e0b75f5241c938137714f8528730bf
Marce <taissir.marce@u-bordeaux.fr>
2026-03-12 22:43:40 +0100

    Particules !

--- de-rat-pigeon/scripts/running.gd
+++ de-rat-pigeon/scripts/running.gd
@@ -10,10 +10,14 @@ func enter(previous_state_path: String, data := {}) -> void:
 	player.audio_player.volume_db= -12
 	player.audio_player.pitch_scale = 2
 	player.audio_player.play()
+	player.particles.show()
 	if abs(player.velocity.x) < player.WALK_SPEED:
 		player.animation_player.play("walk") 
 	else:  
-		player.animation_player.play("run") 
+		print("Run")
+		player.animation_player.play("run")
+		
+		player.particles.play("run")
 
 	
 	# Remet tout bien
@@ -47,13 +51,14 @@ func physics_update(delta: float) -> void:
 		if abs(player.velocity.x) < player.WALK_SPEED:
 			player.animation_player.play("walk") 
 		else:  
-
+			player.particles.play("run")
 			player.animation_player.play("run") 
 
 		
 		
 		if player.is_on_wall_only() and sign(dir) != sign(player.get_wall_normal().x) and player.velocity.x !=0.:
 			# state_print("Jumped from wall")
+			#player.particles.hide()
 			finished.emit(WALL_SLIDING)
 		else:
 			if player.wall_contact_coyote > 0.:
@@ -65,11 +70,14 @@ func physics_update(delta: float) -> void:
 				if player.is_on_wall() and sign(dir) != sign(player.get_wall_normal().x) and player.velocity.x !=0.:
 					state_print("Jumped from wall+floor")
 					player.wall_jump_buffer = player.WALL_JUMP_BUFFER_TIME
+					#player.particles.hide()
 					finished.emit(WALL_SLIDING)
 				else:
 					state_print("Jumped from floor")
+					player.particles.hide()
 					finished.emit(JUMPING)
 			elif (absf(player.velocity.x) < player.SPEED/10.) and (dir == 0.):
+				player.particles.hide()
 				finished.emit(IDLE)
 			if player.is_on_floor():
 				player.floor_coyote = player.FLOOR_COYOTE_TIME
@@ -77,6 +85,7 @@ func physics_update(delta: float) -> void:
 				player.floor_coyote -= delta
 		else: # Player not on the floor
 			state_print("FALLS from RUNNING")
+			player.particles.hide()
 			finished.emit(FALLING)
 		
 		
