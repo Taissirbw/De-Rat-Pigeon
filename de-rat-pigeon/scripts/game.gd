d9049ae1e6dfa5eb4823e42ab8de0f2b03fba98e
comptenintendoamericain-rgb <Lazare.Montreuil@obspm.fr>
2026-04-29 21:50:30 +0200

    Ajout de la music

--- de-rat-pigeon/scripts/game.gd
+++ de-rat-pigeon/scripts/game.gd
@@ -1,4 +1,16 @@
 class_name Game extends Node
-
-
+@export_category("Music")
+@export var Music:AudioStreamWAV
+@export var music_loop =1
 #@onready var _pause_menu := $InterfaceLayer/pause_menu as PauseMenu
+@onready var ambiant_music: AudioStreamPlayer = $Ambiant_Music
+
+func _ready() -> void:
+	ambiant_music.stream = Music
+	ambiant_music.volume_db= -15
+	ambiant_music.play()
+	
+func _on_audio_stream_player_2d_finished() -> void:
+	if music_loop:
+		ambiant_music.play()
+	pass # Replace with function body.
